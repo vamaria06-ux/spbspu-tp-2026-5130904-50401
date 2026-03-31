d6a92fbf12120a5963a64f2dcae34742a2d26579
vamaria06-ux <ulanovamaria0906@yandex.ru>
2026-04-01 00:38:25 +0300

    add comand with note

--- ulanova.maria/T1/note.hpp
+++ ulanova.maria/T1/note.hpp
@@ -15,8 +15,17 @@ namespace ulanova
     std::vector<std::weak_ptr<Note>> links;
   };
   using NotePtr = std::shared_ptr<Note>;
-  using db = std::unordered_map<std::string, NotePtr>;
+  using DB = std::unordered_map<std::string, NotePtr>;
+
+  void create_note(DB& db, const std::string& name);
+  void add_line(DB& db, const std::string& name, const std::string& text);
+  std::vector<std::string> show(const DB& db, const std::string& name);
+  void drop(DB& db, const std::string& name);
+
+  void link(DB& db, const std::string& from, const std::string& to);
+  void halt(DB& db, const std::string& from, const std::string& to);
+  std::vector<std::string> mind(DB& db, const std::string& name);
+  size_t expired(const DB& db, const std::string& name);
+  void refresh(DB& db, const std::string& name);
 }
-
-
 #endif
