f334d28ad5eec6354a8ad2367f085db6f2dbd171
vamaria06-ux <ulanovamaria0906@yandex.ru>
2026-05-24 18:16:46 +0300

    add input wrappers

--- ulanova.maria/T2/data_struct.cpp
+++ ulanova.maria/T2/data_struct.cpp
@@ -1,6 +1,8 @@
 #include "data_struct.hpp"
 
 #include <string>
+#include <istream>
+
 
 bool ulanova::operator<(const DataStruct& lhs, const DataStruct& rhs)
 {
@@ -15,3 +17,32 @@ bool ulanova::operator<(const DataStruct& lhs, const DataStruct& rhs)
   return lhs.key3.size() < rhs.key3.size();
 }
 
+std::istream& ulanova::operator>>(std::istream& in, DelimiterIO&& data)
+{
+  if (!in)
+  {
+    return in;
+  }
+  char current = 0;
+  in >> current;
+  if (current != data.expected)
+  {
+    in.setstate(std::ios_base::failbit);
+  }
+  return in;
+}
+
+std::istream& ulanova::operator>>(std::istream& in, LabelIO&& data)
+{
+  if (!in)
+  {
+    return in;
+  }
+  std::string current;
+  in >> current;
+  if (current != data.expected)
+  {
+    in.setstate(std::ios_base::failbit);
+  }
+  return in;
+}
--- ulanova.maria/T2/data_struct.hpp
+++ ulanova.maria/T2/data_struct.hpp
@@ -13,6 +13,37 @@ namespace ulanova
     std::string key3;
   };
 
+  struct DelimiterIO
+  {
+    char expected;
+  };
+
+  struct LabelIO
+  {
+    std::string expected;
+  };
+
+  struct UllLiteralIO
+  {
+    unsigned long long& value;
+  };
+
+  struct DblScientificIO
+  {
+    double& value;
+  };
+
+  struct StringIO
+  {
+    std::string& value;
+  };
+
+  std::istream& operator>>(std::istream& in, DelimiterIO&& data);
+  std::istream& operator>>(std::istream& in, LabelIO&& data);
+  std::istream& operator>>(std::istream& in, UllLiteralIO&& data);
+  std::istream& operator>>(std::istream& in, DblScientificIO&& data);
+  std::istream& operator>>(std::istream& in, StringIO&& data);
+
   std::istream& operator>>(std::istream& in, DataStruct& data);
   std::ostream& operator<<(std::ostream& out, const DataStruct& data);
 
