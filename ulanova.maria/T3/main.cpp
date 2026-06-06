f3d2dd633318e124580137b29236c08c3403235b
vamaria06-ux <ulanovamaria0906@yandex.ru>
2026-06-06 18:32:27 +0300

    initial file

--- ulanova.maria/T3/main.cpp
+++ ulanova.maria/T3/main.cpp
@@ -1,4 +1,20 @@
 #include <iostream>
+#include <fstream>
 
-int main()
-{}
+int main(int argc, char** argv)
+{
+  if (argc > 2)
+  {
+    std::cerr << "wrong number of arguments\n";
+    return 1;
+  }
+
+  std::ifstream input(argv[1]);
+  if (!input)
+  {
+    std::cerr << "cannot open file\n";
+    return 1;
+  }
+
+  return 0;
+}
