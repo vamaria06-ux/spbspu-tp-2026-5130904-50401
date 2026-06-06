517ae290dc7e0376470ffa3dd08db2c7241d0238
vamaria06-ux <ulanovamaria0906@yandex.ru>
2026-06-06 20:32:38 +0300

    add command table

--- ulanova.maria/T3/main.cpp
+++ ulanova.maria/T3/main.cpp
@@ -2,6 +2,26 @@
 
 #include <iostream>
 #include <fstream>
+#include <limits>
+#include <map>
+#include <string>
+
+namespace
+{
+  using polygons_t = std::vector< ulanova::Polygon >;
+  using command_t = void (*)(std::istream &, std::ostream &, const polygons_t &);
+
+  void skipLine(std::istream & in)
+  {
+    in.clear();
+    in.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+  }
+
+  void doCount(std::istream &, std::ostream &, const polygons_t &)
+  {
+    throw std::logic_error("COUNT is not implementd");
+  }
+}
 
 int main(int argc, char ** argv)
 {
@@ -20,6 +40,23 @@ int main(int argc, char ** argv)
 
   std::vector < ulanova::Polygon > polygons;
   ulanova::readPolygons(input, polygons);
+
+  std::map< std::string, command_t > commands;
+  commands["COUNT"] = doCount;
+
+  std::string command;
+  while (std::cin >> command)
+  {
+    try
+    {
+      commands.at(command)(std::cin, std::cout, polygons);
+    }
+    catch(const std::exception &)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+      skipLine(std::cin);
+    }
+  }
 
   return 0;
 }
